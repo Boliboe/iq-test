16c91df4e8fbe00898b4e70604b57aaa27d4f365
Boliboe <iamjohnsomto@gmail.com>
2023-06-19 22:59:54 +0100

    quizbox, next btn and option updated on sandbox.js file

--- js/sandbox.js
+++ js/sandbox.js
@@ -19,4 +19,32 @@ exitBtn.onclick = () =>{
 continueBtn.onclick = () =>{
     infoBox.classList.remove("activeInfo"); //hide the info box
     quizBox.classList.add("activeQuiz"); //show the quiz box
+    showQuestions(0);
+}
+
+let queCount = 0;
+
+const nextBtn = quizBox.querySelector(".next-btn");
+
+//if Next Button is Clicked
+nextBtn.onclick = () => {
+    if(queCount < questions.length - 1){
+        queCount++;
+        showQuestions(queCount);
+    }else{
+        console.log("Questions completed")
+    }
+}
+
+//getting questions and options from array
+function showQuestions(index){
+    const queText = document.querySelector(".que-text");
+    const optionList = document.querySelector(".option-list");
+    let queTag = '<span>' + questions[index].question + '</span>';
+    let optionTag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
+                    + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+    queText.innerHTML = queTag;
+    optionList.innerHTML = optionTag;
 }
