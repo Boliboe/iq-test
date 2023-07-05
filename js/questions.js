d309400a6399e6b9c229fb15b54811bf24f8a7f5
Boliboe <iamjohnsomto@gmail.com>
2023-07-05 21:48:24 +0100

    questions added

--- js/questions.js
+++ js/questions.js
@@ -2,57 +2,222 @@
 let questions = [
     {
         numb: 1,
-        question: "What does HTML stand for?",
-        answer:"Hyper Text Markup Language",
+        question: "In 1973, a cloud of acid rain spread over Europe due to the eruption of a volcano in ...?",
+        answer:"Iceland",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Linguistics",
-            "Hyper Test Markup Language",
-            "Hyper Text Markup Language"
+            "Iceland",
+            "Prussia",
+            "Spain",
+            "Sicily"
         ]
     },
     {
         numb: 2,
-        question: "What does CSS stand for?",
-        answer:"Cascading Style Sheets",
+        question: "Who wrote the Waverly novels?",
+        answer:"Walter Scott",
         options: [
-            "Continuous Style Source",
-            "Cascading Style Sheets",
-            "Counter Style System",
-            "Computer Style Sheets"
+            "John Milton",
+            "Gustave Flaubert",
+            "John Banim",
+            "Walter Scott"
         ]
     },
     {
         numb: 3,
-        question: "What does PHP stand for?",
-        answer:"Hyper Text Preprocessor",
+        question: "Most jet airlines are powered by ...?",
+        answer:"Turbofans",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Protocol",
-            "Package High Processor",
-            "Process Hypertext Protocol"
+            "Ramjets",
+            "Turboprops",
+            "Turbofans",
+            "Pulsejets"
         ]
     },
     {
         numb: 4,
-        question: "What does HTML stand for?",
-        answer:"Hyper Text Markup Language",
+        question: "What is perovskite?",
+        answer:"Mineral",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Linguistics",
-            "Hyper Test Markup Language",
-            "Hyper Text Markup Language"
+            "Mid-ocean ridge",
+            "Metabolite",
+            "Mineral",
+            "Telescope"
         ]
     },
     {
         numb: 5,
-        question: "What does HTML stand for?",
-        answer:"Hyper Text Markup Language",
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Linguistics",
-            "Hyper Test Markup Language",
-            "Hyper Text Markup Language"
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of these processes is not a form of convection?",
+        answer:"Metal expansion",
+        options: [
+            "Atmospheric circulation",
+            "Oceanic circulation",
+            "Foehn wind",
+            "Metal expansion"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which animal does not fit into the list?",
+        answer:"Rabbit",
+        options: [
+            "Chipmunk",
+            "Sqirrel",
+            "Rabbit",
+            "Guinea pig"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the national intelligence agency of Israel?",
+        answer:"Mossad",
+        options: [
+            "Mizrachi",
+            "Mossad",
+            "Herut",
+            "Mabahith"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Seaweed is composed of ...?",
+        answer:"Algae",
+        options: [
+            "Algae",
+            "Fungi",
+            "Sponges",
+            "Grasses"
+        ]
+    },
+    {
+        numb: 10,
+        question: "To which crystal family does quartz belong?",
+        answer:"Hexagonal",
+        options: [
+            "Hexogonal",
+            "Isometric",
+            "Tetragonal",
+            "Monoclinic"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In what year was the dwarf planet named Pluto discovered?",
+        answer:"1930",
+        options: [
+            "1975",
+            "1969",
+            "2001",
+            "1930"
         ]
     },
 
